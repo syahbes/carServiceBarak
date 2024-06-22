382ce59dcc97f35936fc9c14697bad3dc16d17da
Shlomi Yahbes <syahbes@gmail.com>
2024-06-22 19:03:09 +0300

    reorder

--- App.js
+++ App.js
@@ -1,30 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import MyCarLocation from "./src/screens/MyCarLocation";
-import SearchCity from "./src/screens/SearchCity";
-import { useState } from "react";
+import MyCarLocationMain from "./src/screens/MyCarLocation";
 
 export default function App() {
-  const [showSearchCity, setShowSearchCity] = useState(false);
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar hidden />
-      {showSearchCity ? (
-        <SearchCity setShowSearchCity={setShowSearchCity} />
-      ) : (
-        <MyCarLocation setShowSearchCity={setShowSearchCity} />
-      )}
-    </SafeAreaView>
-  );
+  return <MyCarLocationMain />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "100%",
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
